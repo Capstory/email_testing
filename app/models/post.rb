0f9fbe4b3d9d0d7b4f0cd68baeac0bf0cbe9e448
Brad Arner <arner.john@gmail.com>
2013-10-27 20:34:42 -0400

    Adding small photo size to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :email, :image, :capsule_id
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "200x200>" }
   
   belongs_to :capsule
 end
