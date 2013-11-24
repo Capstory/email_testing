bfa75d1a226454804c4c050b62f13a07f385281a
Brad Arner <arner.john@gmail.com>
2013-11-24 13:56:41 -0500

    Remove email validation for capsules and unnecessary buttons in capsule

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -6,7 +6,7 @@ class Capsule < ActiveRecord::Base
   has_many :posts
   
   validates_uniqueness_of :email
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  # validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   
   extend FriendlyId
   friendly_id :named_url
