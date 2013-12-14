0eebcad3be4a8e4eb9ee6d94838f704458993b1e
Brad Arner <arner.john@gmail.com>
2013-12-13 19:00:21 -0500

    Changing an html_safe declaration to email response

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -11,7 +11,7 @@ class PostMailer < ActionMailer::Base
   def day_two_reminder(sender, capsule_email, capsule_link)
     @sender_email = sender
     @capsule_email = capsule_email
-    @url = capsule_link.is_a?(String) ? "http://www.capstory.me/" + capsule_link : "http://www.capstory.me/capsules/" + capsule_link.to_s
+    @url = capsule_link.is_a?(String) ? "http://www.capstory.me/" + capsule_link.html_safe : "http://www.capstory.me/capsules/" + capsule_link.to_s
     
     mail(from: @capsule_email, reply_to: @capsule_email, to: @sender_email, subject: "Thank You!")
   end
