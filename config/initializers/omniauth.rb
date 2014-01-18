e525c0c1ec59256e189f2dcb11aa815a331df4fa
Brad Arner <arner.john@gmail.com>
2014-01-18 13:06:00 -0500

    Adding scope to facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ OmniAuth.config.on_failure = Proc.new { |env|
    }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '657512180934505', 'ba2bce51473acb13c1b46a348d869392', display: 'popup'
+  provider :facebook, '657512180934505', 'ba2bce51473acb13c1b46a348d869392', display: 'popup', scope: "email, publish_actions"
   provider :twitter, 'KQZXkv7VAiRdU4nVkdrkbw', 'tfITw4tArdJRgwdCzHs8iAwxaQls4wPT7i7vPTCLA'
   provider :identity, :fields => [:email, :name, :genre, :event_date], on_failed_registration: lambda { |env|
     env["rack.session"][:identity] = env['omniauth.identity']
