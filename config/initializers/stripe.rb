b0d68e8c70fa510f13e9e209677fa7617f0edbc6
Brad Arner <arner.john@gmail.com>
2014-02-16 14:00:09 -0500

    Changing flow control stripe.rb initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,13 @@
-Rails.configuration.stripe = {
-  if !Rails.env.production?
-    publishable_key: 'pk_test_kHdS86KoSOZw7gp811b2qCxj',
-    secret_key: 'sk_test_DfmIwlYRxxBoM2GWapZ8XJkp'
-  else
-    publishable_key: ENV["STRIPE_PK"],
-    secret_key: ENV["STRIPE_SECRET"]
-  end
-}
+if Rails.env.production?
+  Rails.configuration.stripe = {
+      publishable_key: ENV["STRIPE_PK"],
+      secret_key: ENV["STRIPE_SECRET"]
+  }
+else
+  Rails.configuration.stripe = {
+      publishable_key: 'pk_test_kHdS86KoSOZw7gp811b2qCxj',
+      secret_key: 'sk_test_DfmIwlYRxxBoM2GWapZ8XJkp'
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
