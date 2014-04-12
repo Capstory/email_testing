b57adf0ed9089d57bc875604500752883a8e2728
Brad Arner <arner.john@gmail.com>
2014-04-11 21:28:40 -0400

    Trying to get foundation 5 working in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ EmailTesting::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ["application.css", "static.css", "charge.css", "application.js", "static.js", "capsule.js", "vendor.css", "vendor.js", "homepage.css", "homepage.js"]
+  config.assets.precompile += ["application.css", "static.css", "charge.css", "application.js", "static.js", "capsule.js", "vendor.css", "vendor.js", "homepage.css", "homepage.js", "modernizr.js"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
