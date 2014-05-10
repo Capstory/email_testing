94ec8bbacde644e3a30a8b19b6bea0f5c45b347c
Brad Arner <arner.john@gmail.com>
2014-05-10 10:26:09 -0400

    Removing the redundant .modal_img js

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,11 @@ OmniAuth.config.on_failure = Proc.new { |env|
    }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '657512180934505', 'ba2bce51473acb13c1b46a348d869392', scope: "email,publish_actions"
+  if Rails.env.production?
+    provider :facebook, '657512180934505', 'ba2bce51473acb13c1b46a348d869392', scope: "email,publish_actions"
+  else
+    provider :facebook, "774362285916160", "f6108f85cbc187652240139f061bd0e9", scope: "email, publish_actions"
+  end
   provider :twitter, 'KQZXkv7VAiRdU4nVkdrkbw', 'tfITw4tArdJRgwdCzHs8iAwxaQls4wPT7i7vPTCLA'
   provider :identity, :fields => [:email, :name, :user_id], on_failed_registration: lambda { |env|
     env["rack.session"][:identity] = env['omniauth.identity']
