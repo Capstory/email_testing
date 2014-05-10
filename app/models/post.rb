79b91e6ed28e43ea3cb8f4ec0153b956f1096056
Brad Arner <arner.john@gmail.com>
2014-05-09 23:06:05 -0400

    Adding a large style size to paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :email, :image, :capsule_id, :filepicker_url
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "200x200>" }
+  has_attached_file :image, :styles => { :large => "450x450>", :medium => "300x300>", :thumb => "100x100>", :small => "200x200>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   # validates_attachment_file_name :image, matches: [/./]
   # validates_attachment_size :image, greater_than: 10.kilobytes
