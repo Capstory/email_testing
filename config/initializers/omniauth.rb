f5d84622ca9d598f2c050d83ef3442592d28ec93
Brad Arner <arner.john@gmail.com>
2014-05-13 19:12:35 -0400

    Adding facebook app for staging environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,6 +7,8 @@ OmniAuth.config.on_failure = Proc.new { |env|
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     provider :facebook, '657512180934505', 'ba2bce51473acb13c1b46a348d869392', scope: "email,publish_actions"
+  elsif Rails.env.staging?
+    provider :facebook, '776372492381806', 'd752c0e78f2cc749749433a1d29f604d', scope: "email, publish_actions"
   else
     provider :facebook, "774362285916160", "f6108f85cbc187652240139f061bd0e9", scope: "email, publish_actions"
   end
