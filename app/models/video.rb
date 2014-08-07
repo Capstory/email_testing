eb3fef24ba33b253e58db9e62a995cafff60e078
Brad Arner <arner.john@gmail.com>
2014-08-06 22:05:56 -0400

    Adding a manual video upload process

--- app/models/video.rb
+++ app/models/video.rb
@@ -75,4 +75,79 @@ class Video < ActiveRecord::Base
     name = self.video_file_name.delete(Pathname.new(self.video_file_name).extname)
     return name
   end
+
+	# Needed options
+	# options[:input_url] => The url for the original file on S3
+	# options[:video_filename] => the general desired filename w/o extension
+	# options[:capsule_id] => The ID of the desired capsule
+	# options[:body] => Default should be 'No message'
+	# options[:email] => Sender email - manual should default to brad@capstory.me
+	#
+	def	self.manually_generate_file(options={})
+    video_file = Video.create
+    # Create the Zencoder Job, which Zencoder than works in the background to convert and put in the specified location
+    zjob = Zencoder::Job.create({
+      input: options[:input_url],
+      outputs: [
+        {
+          :filename => "#{options[:video_filename]}.mp4",
+          :base_url => "s3://manual_videos/#{video_file.id}/videos/",
+          :public => true,
+          :size => "640x480",
+          :apsect_mode => "pad",
+          :thumbnails => {
+            :number => 1,
+            :format => 'jpg',
+            :width => 500,
+            :height => 500,
+            :aspect_mode => 'crop',
+            :base_url => "s3://manual_videos/#{video_file.id}/thumb/",
+            :filename => "#{options[:video_filename]}",
+            :public => true
+          }
+        },
+        {
+          :filename => "#{options[:video_filename]}.ogg",
+          :base_url => "s3://manual_videos/#{video_file.id}/videos/",
+          :format => 'ogg',
+          :public => true,
+          :size => "640x480",
+          :aspect_mode => "pad"
+        }
+      ]
+    })
+
+    
+    # Waiting until video conversion is complete
+    state = "in progress"
+    until state == "finished" do
+			puts "Still working..."
+			puts "Zobdy id: #{zjob.body}"
+			# p Zencoder::Job.details(zjob.body["id"])
+      @details = Zencoder::Job.details(zjob.body['id'])
+      state = @details.body['job']['state']
+      sleep 5
+    end
+    
+    # Creating Post that will hold the video instance
+    # AWS.config({
+    #   access_key_id: 'AKIAI32PTERUQJYBQUDA',
+    #   secret_access_key: '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'
+    # })
+    
+    @post = Post.create do |post|
+      post.capsule_id = options[:capsule_id]
+      url = URI.parse(@details.body['job']['thumbnails'].first['url'])
+      new_url = "http://" + url.host + url.path
+      post.image = URI.parse(new_url)
+      post.body = options[:body]
+      post.email = options[:email]
+    end
+    
+    # Setting final parameters for video instance
+    video_file.zencoder_url = zjob.body['outputs'].first['url']
+    video_file.post_id = @post.id
+    video_file.save
+		
+	end
 end
