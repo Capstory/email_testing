7bf300fe56a4b101c1c558eef6b0dc7a3a365c40
Brad Arner <arner.john@gmail.com>
2014-10-08 08:49:50 -0400

    Putting the force_ssl back in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 	# before_filter :show_request_env_variables
-	# force_ssl if: :in_production?
+	force_ssl if: :in_production?
 	# before_filter :resolve_url
 
 	private
