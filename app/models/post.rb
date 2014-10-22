6e996414809b2a1e86eed70407cfc40bc4d83d95
Brad Arner <arner.john@gmail.com>
2014-10-22 14:57:36 -0400

    Making sure posts with tag_for_deletion dont appear

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :email, :image, :capsule_id, :filepicker_url, :tag_for_deletioni, :verified
+  attr_accessible :body, :email, :image, :capsule_id, :filepicker_url, :tag_for_deletion, :verified
 
   has_attached_file :image, :styles => { :large => "450x450>", :medium => "300x300>", :thumb => "100x100>", :small => "200x200>", :capsule_height => "x350", :capsule_width => "350x", :lightbox_width => "1024x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
@@ -9,6 +9,6 @@ class Post < ActiveRecord::Base
   belongs_to :capsule
   has_one :video
 
-	scope :verified, -> { where(verified: true) }
+	scope :verified, -> { where(verified: true, tag_for_deletion: false) }
 	scope :not_yet_verified, -> { where(verified: false) }
 end
