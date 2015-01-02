298002b8021e926b3389867a0b7f99fe26654c49
Brad Arner <arner.john@gmail.com>
2015-01-02 16:44:44 -0500

    Removing angular_capsule/services and the angular-isotope directives

--- app/assets/javascripts/angular_capsule.js
+++ app/assets/javascripts/angular_capsule.js
@@ -7,11 +7,11 @@
 //= require ./angular_src_files/angular-touch
 //= require ./angular_capsule/angular_capsule.js.erb
 //= require_tree ./angular_capsule/controllers
-//= require_tree ./angular_capsule/services
+// require_tree ./angular_capsule/services
 //= require_tree ./angular_capsule/models
 //= require ./angular_capsule/filters.js
 //= require ./angular_capsule/directives.js
-//= require ./common_use/angular-isotope.js
+// require ./common_use/angular-isotope.js
 //= require ./common_use/ng-infinite-scroll.js
 //= require_self
 
