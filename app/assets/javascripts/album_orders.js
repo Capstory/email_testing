4b52ef4141fd3659a625447ebc13fc1896c55bf3
Brad Arner <arner.john@gmail.com>
2015-06-30 15:47:13 -0400

    Unlinking angular album_order dirs without files

--- app/assets/javascripts/album_orders.js
+++ app/assets/javascripts/album_orders.js
@@ -7,8 +7,8 @@
 // require ./angular_src_files/angular-cookies
 //= require ./album_orders/app.js.erb
 //= require_tree ./album_orders/controllers
-//= require_tree ./album_orders/services
-//= require_tree ./album_orders/models
+// require_tree ./album_orders/services
+// require_tree ./album_orders/models
 // require ./album_orders/filters.js
 //= require ./album_orders/directives.js
 // require string
